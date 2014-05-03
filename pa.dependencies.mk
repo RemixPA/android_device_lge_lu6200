b2a24bfc27f9ec19f8d2eaff8614a840d4b1f390
xxxifan <445947962@qq.com>
2014-05-03 14:59:21 +0800

    init lu6200 support

---
+++ pa.dependencies.mk
@@ -0,0 +1,20 @@
+[
+ {
+	"remote":       "github",
+	"repository":   "Cyanogenmod/android_device_lge_iprj-common",
+	"target_path":  "device/lge/iprj-common",
+	"revision":     "cm-11.0"
+ },
+ {
+	"remote":       "github",
+	"repository":   "RemixPA/vendor_lge",
+	"target_path":  "vendor/lge",
+	"revision":     "kk4.4"
+ },
+ {
+	"remote":	"github",
+	"repository": 	"Mokee/android_kernel_lge_iproj",
+	"target_path": 	"kernel/lge/iproj",
+	"revision":	"kk_mkt"
+ }
+]
